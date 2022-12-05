ab147a623ccb637a2cceb0e70a34d1a450c5485f
Dez <warningdez1@gmail.com>
2022-12-05 09:12:06 -0500

    re-styled browse cocktails page,  mobile responsive css tweaks, and redux implemented using incrementation of the count. Next: decrement count using redux.

--- src/components/BrowseCocktails.js
+++ src/components/BrowseCocktails.js
@@ -25,30 +25,6 @@ const BrowseCocktails = () => {
   const [cocktailImage3, setCocktailImage3] = useState("")
   const [cocktailInstructions3, setCocktailInstructions3] = useState("")
 
-  const [cocktailTitle4, setCocktailTitle4] = useState("")
-  const [cocktailImage4, setCocktailImage4] = useState("")
-  const [cocktailInstructions4, setCocktailInstructions4] = useState("")
-  
-  const [cocktailTitle5, setCocktailTitle5] = useState("")
-  const [cocktailImage5, setCocktailImage5] = useState("")
-  const [cocktailInstructions5, setCocktailInstructions5] = useState("")
-
-  const [cocktailTitle6, setCocktailTitle6] = useState("")
-  const [cocktailImage6, setCocktailImage6] = useState("")
-  const [cocktailInstructions6, setCocktailInstructions6] = useState("")
-
-  const [cocktailTitle7, setCocktailTitle7] = useState("")
-  const [cocktailImage7, setCocktailImage7] = useState("")
-  const [cocktailInstructions7, setCocktailInstructions7] = useState("")
-
-  const [cocktailTitle8, setCocktailTitle8] = useState("")
-  const [cocktailImage8, setCocktailImage8] = useState("")
-  const [cocktailInstructions8, setCocktailInstructions8] = useState("")
-
-  const [cocktailTitle9, setCocktailTitle9] = useState("")
-  const [cocktailImage9, setCocktailImage9] = useState("")
-  const [cocktailInstructions9, setCocktailInstructions9] = useState("")
-
   const fetchCocktailByID = async() => {
 
     const cocktailInfo1 = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${count}`)
@@ -81,70 +57,11 @@ const BrowseCocktails = () => {
     setCocktailInstructions3(cocktailDescription3)
 
 
-    const cocktailInfo4 = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${count+3}`)
-    const cocktailData4 = await cocktailInfo4.json()
-    const cocktailName4 = cocktailData4.drinks[0].strDrink
-    const cocktailImg4 = cocktailData4.drinks[0].strDrinkThumb
-    const cocktailDescription4 = cocktailData4.drinks[0].strInstructions
-    setCocktailTitle4(cocktailName4)
-    setCocktailImage4(cocktailImg4)
-    setCocktailInstructions4(cocktailDescription4)
-
-
-    const cocktailInfo5 = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${count+4}`)
-    const cocktailData5 = await cocktailInfo5.json()
-    const cocktailName5 = cocktailData5.drinks[0].strDrink
-    const cocktailImg5 = cocktailData5.drinks[0].strDrinkThumb
-    const cocktailDescription5 = cocktailData5.drinks[0].strInstructions
-    setCocktailTitle5(cocktailName5)
-    setCocktailImage5(cocktailImg5)
-    setCocktailInstructions5(cocktailDescription5)
-
-
-    const cocktailInfo6 = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${count+5}`)
-    const cocktailData6 = await cocktailInfo6.json()
-    const cocktailName6 = cocktailData6.drinks[0].strDrink
-    const cocktailImg6 = cocktailData6.drinks[0].strDrinkThumb
-    const cocktailDescription6 = cocktailData6.drinks[0].strInstructions
-    setCocktailTitle6(cocktailName6)
-    setCocktailImage6(cocktailImg6)
-    setCocktailInstructions6(cocktailDescription6)
-
-
-    const cocktailInfo7 = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${count+6}`)
-    const cocktailData7 = await cocktailInfo7.json()
-    const cocktailName7 = cocktailData7.drinks[0].strDrink
-    const cocktailImg7 = cocktailData7.drinks[0].strDrinkThumb
-    const cocktailDescription7 = cocktailData7.drinks[0].strInstructions
-    setCocktailTitle7(cocktailName7)
-    setCocktailImage7(cocktailImg7)
-    setCocktailInstructions7(cocktailDescription7)
-
-
-    const cocktailInfo8 = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${count+7}`)
-    const cocktailData8 = await cocktailInfo8.json()
-    const cocktailName8 = cocktailData8.drinks[0].strDrink
-    const cocktailImg8 = cocktailData8.drinks[0].strDrinkThumb
-    const cocktailDescription8 = cocktailData8.drinks[0].strInstructions
-    setCocktailTitle8(cocktailName8)
-    setCocktailImage8(cocktailImg8)
-    setCocktailInstructions8(cocktailDescription8)
-
-
-    const cocktailInfo9 = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${count+8}`)
-    const cocktailData9 = await cocktailInfo9.json()
-    const cocktailName9 = cocktailData9.drinks[0].strDrink
-    const cocktailImg9 = cocktailData9.drinks[0].strDrinkThumb
-    const cocktailDescription9 = cocktailData9.drinks[0].strInstructions
-    setCocktailTitle9(cocktailName9)
-    setCocktailImage9(cocktailImg9)
-    setCocktailInstructions9(cocktailDescription9)
-
 
 
   }
   return (
-    <>
+    <div onLoad={fetchCocktailByID()}>
 
     <Row xs={1} md={3} className="g-4">
 
@@ -184,85 +101,13 @@ const BrowseCocktails = () => {
           </Card>
         </Col>
 
-        <Col>
-          <Card className='card-container'>
-            <Card.Img className='card-image' variant="top" src={cocktailImage4} />
-            <Card.Body>
-              <Card.Title>{cocktailTitle4}</Card.Title>
-              <Card.Text>
-                {cocktailInstructions4}
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-
-        <Col>
-          <Card className='card-container'>
-            <Card.Img className='card-image' variant="top" src={cocktailImage5} />
-            <Card.Body>
-              <Card.Title>{cocktailTitle5}</Card.Title>
-              <Card.Text>
-                {cocktailInstructions5}
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-
-        <Col>
-          <Card className='card-container'>
-            <Card.Img className='card-image' variant="top" src={cocktailImage6} />
-            <Card.Body>
-              <Card.Title>{cocktailTitle6}</Card.Title>
-              <Card.Text>
-                {cocktailInstructions6}
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-
-        <Col>
-          <Card className='card-container'>
-            <Card.Img className='card-image' variant="top" src={cocktailImage7} />
-            <Card.Body>
-              <Card.Title>{cocktailTitle7}</Card.Title>
-              <Card.Text>
-                {cocktailInstructions7}
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-
-        <Col>
-          <Card className='card-container'>
-            <Card.Img className='card-image' variant="top" src={cocktailImage8} />
-            <Card.Body>
-              <Card.Title>{cocktailTitle8}</Card.Title>
-              <Card.Text>
-                {cocktailInstructions8}
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-
-        <Col>
-          <Card className='card-container'>
-            <Card.Img className='card-image' variant="top" src={cocktailImage9} />
-            <Card.Body>
-              <Card.Title>{cocktailTitle9}</Card.Title>
-              <Card.Text>
-                {cocktailInstructions9}
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
 
     </Row>
-<button onClick={fetchCocktailByID}>clg name</button>
-<button onClick={()=>dispatch(incrementCount(1))}>Increment</button>
-<br/>
-<br/>
-<br/>
-    </>
+    <button onClick={()=>dispatch(incrementCount(3))}>Increment</button>
+    <br/>
+    <br/>
+    <br/>
+    </div>
   )
 }
 
